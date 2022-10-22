c1e802503a8cf3969e802b0b0508e1ea4f6ee91b
superrednova42 <39013503+superrednova42@users.noreply.github.com>
2022-10-21 20:59:13 -0700

    Nearly there, a little more debugging

--- utils/ItemsExport.py
+++ utils/ItemsExport.py
@@ -202,14 +202,19 @@ def _export_item_FBX(item: ExportedItem) -> None:
                 eval_obj = depsgraph.objects[obj.name]
                 if bm not in eval_obj.data.uv_layers and bm in eval_obj.data.attributes:
                     copy = obj.copy()
-                    replaced_objects.append(copy)
+                    copy.data = bpy.data.meshes.new_from_object(eval_obj)
+                    replaced_objects.append((obj,copy))
                     copy.data.attributes.remove(copy.data.attributes[bm])
                     copy.data.uv_layers.active = copy.data.uv_layers.new(name=bm)
                     newuv = [uv.vector.xy for uv in eval_obj.data.attributes[bm].data]
                     for loop in copy.data.loops:
                         copy.data.uv_layers[bm].data[loop.index].uv = newuv[loop.index]
-                    item.objects.remove(obj)
-                    item.objects.append(copy)
+                    bpy.context.scene.collection.objects.link(copy)
+    
+    for objects in replaced_objects:
+        item.objects.remove(objects[0])
+        item.objects.append(objects[1])
+    
     
     deselect_all_objects()
     for obj in item.objects:
@@ -218,8 +223,8 @@ def _export_item_FBX(item: ExportedItem) -> None:
 
     bpy.ops.export_scene.fbx(**exportArgs) #one argument is optional, so give a modified dict and **unpack
     
-    for obj in replaced_objects:
-        bpy.data.meshes.remove(obj.data)
+    for objects in replaced_objects:
+        bpy.data.meshes.remove(objects[1].data)
 
     deselect_all_objects()
 
